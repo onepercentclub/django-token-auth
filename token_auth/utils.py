f97066fa1da44b8721c7c766a2959789227ccd78
Loek van Gent <loek@1procentclub.nl>
2015-05-14 15:56:42 +0200

    Add utility command to genreate token

--- token_auth/utils.py
+++ token_auth/utils.py
@@ -1,7 +1,11 @@
-
 import os
 
-from contextlib import contextmanager
+from datetime import datetime, timedelta
+import base64
+import hashlib
+import hmac
+from Crypto.Cipher import AES
+from Crypto import Random
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
@@ -39,3 +43,37 @@ def get_token_settings(key=None):
         return token_auth_settings
     except AttributeError:
         raise ImproperlyConfigured("TOKEN_AUTH missing in settings.")
+
+
+def generate_token(email, username, first_name, last_name):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    message = 'time={0}|username={1}|name={2} {3}|' \
+              'email={4}'.format(timestamp, username, first_name, last_name, email)
+    aes_message, hmac_digest = _encode_message(message)
+    token = base64.urlsafe_b64encode(aes_message + hmac_digest.digest())
+    return token
+
+
+def _encode_message(message):
+    """
+    Helper method which returns an encoded version of the
+    message passed as an argument.
+
+    It returns a tuple containing a string formed by two elements:
+
+    1. A string formed by the initialization vector and the AES-128
+    encrypted message.
+    2. The HMAC-SHA1 hash of that string.
+    """
+    aes_key = get_token_settings('aes_key')
+    hmac_key = get_token_settings('hmac_key')
+
+    pad = lambda s: s + (AES.block_size - len(s) % AES.block_size) * chr(
+        AES.block_size - len(s) % AES.block_size)
+    init_vector = Random.new().read(AES.block_size)
+    cipher = AES.new(aes_key, AES.MODE_CBC, init_vector)
+    padded_message = pad(message)
+    aes_message = init_vector + cipher.encrypt(padded_message)
+    hmac_digest = hmac.new(hmac_key, aes_message, hashlib.sha1)
+
+    return aes_message, hmac_digest
