b045e20ab817676fa005bbecca36b0b5df22ac61
Loek van Gent <loek@onepercentclub.com>
2016-05-13 12:28:40 +0200

    Fix setup.

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
 
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+
 setuptools.setup(
     name="django-token-auth",
     version=token_auth.__version__,
@@ -22,16 +23,26 @@ setuptools.setup(
     author_email="devteam@onepercentclub.com",
     install_requires=[
         'Django>=1.6.8',
+        'pycrypto>=2.6.1',
         'python-saml==2.1.4'
     ],
     tests_require={
-        'factory-boy>=2.6.0',
-        'bunch==1.0.1',
-        'django-nose>=1.4',
-        'django-setuptest>=0.1.4',
-        'mock>=1.0.1',
-        'djangorestframework>=2.3.14'
+        'django_nose>=1.4',
+        'factory-boy>=2.3.1',
+        'django-setuptest==0.1.4',
+        'mock==1.0.1',
     },
-    test_suite = "token_auth.runtests.runtests"
+    test_suite = "token_auth.runtests.runtests",
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: None',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
 )
+
 
