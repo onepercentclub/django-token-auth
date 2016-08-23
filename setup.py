1b92970e0c90dfa42d2db314a643f7aff81e9e78
Ernst Odolphi <ernst@onepercentclub.com>
2016-08-23 09:32:55 +0200

    Try to fix core dump

--- setup.py
+++ setup.py
@@ -23,7 +23,8 @@ setuptools.setup(
     install_requires=[
         'Django>=1.6.8',
         'pycrypto>=2.6.1',
-        'python-saml==2.1.7'
+        'python-saml==2.1.7',
+        'lxml==3.6.1'
     ],
     tests_require={
         'django_nose>=1.4',
